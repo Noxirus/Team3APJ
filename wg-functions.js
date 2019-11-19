11c04cd641e6abceb60a1ee7d11ebae93d4e7430
SinKane <wade.grimm@shaw.ca>
2019-11-19 09:55:01 -0700

    formatting

--- wg-functions.js
+++ wg-functions.js
@@ -6,54 +6,40 @@ var myReg = /^[ABCEGHJ-NPRSTVXY][0-9][ABCEGHJ-NPRSTV-Z][ ]?[0-9][ABCEGHJ-NPRSTV-
 //Functions Start
 
 
-function fnValidatePostal(postal)
-{
-    if (myReg.test(postal))
-    {
+function fnValidatePostal(postal) {
+    if (myReg.test(postal)) {
         return true;
-    }
-    else
-    {
+    } else {
         return false;
     }
 }
 
 
-function fnFormValidate()	
-{
+function fnFormValidate() {
     var myform = document.forms.namedItem("regForm");
     var focusSet = false;
-    for (i=0; i < myform.elements.length; i++)
-    {
-        if (myform.elements[i].type == "text" || myform.elements[i].type == "email" || myform.elements[i].type == "password" || myform.elements[i].type == "select-one" )
-        {
-            if (myform.elements[i].value == "")
-            {
-                if (focusSet == false)
-                {
+    for (i = 0; i < myform.elements.length; i++) {
+        if (myform.elements[i].type == "text" || myform.elements[i].type == "email" || myform.elements[i].type == "password" || myform.elements[i].type == "select-one") {
+            if (myform.elements[i].value == "") {
+                if (focusSet == false) {
                     myform.elements[i].focus();
                     focusSet = true;
                 }
                 myform.elements[i].style.backgroundColor = "#f0cccc";
-            }
-            else{
+            } else {
                 focusSet = false;
                 myform.elements[i].style.backgroundColor = "";
             }
         }
     }
-    
-    if (focusSet)
-    {
+
+    if (focusSet) {
         focusSet = false;
         return false;
-    }
-    else
-    {
-        if (fnValidatePostal(document.getElementById("inputpostal").value)){
+    } else {
+        if (fnValidatePostal(document.getElementById("inputpostal").value)) {
             return confirm("Continue submitting?");
-        }
-        else{
+        } else {
             document.getElementById("inputpostal").style.backgroundColor = "#f0cccc";
             document.getElementById("inputpostal").focus();
             return false;
@@ -62,12 +48,10 @@ function fnFormValidate()
 }
 
 
-function fnClearForm()
-{ 
+function fnClearForm() {
     var myform = document.forms.namedItem("regForm");
-    for (i=0; i < myform.elements.length; i++)
-    { 
-        myform.elements[i].style.backgroundColor = ""; 
+    for (i = 0; i < myform.elements.length; i++) {
+        myform.elements[i].style.backgroundColor = "";
         myform.elements[i].value = "";
     }
 }
